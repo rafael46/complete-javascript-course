267369a8ba43c87723024541735737b77649bd15
Rafael Gdl <rafael@vipmarketingla.comcls>
2018-12-18 13:32:21 -0800

    functions closure

---
+++ functions/functions.js
@@ -0,0 +1,19 @@
+
+// The 7 ways of the function
+//https://javascript.christmas/2018/13
+
+function counter() {
+  let count = 0;
+
+  function increment() {
+    return ++count;
+  }
+
+  return increment;
+}
+
+const coolClosureCounter = counter();
+
+console.log(coolClosureCounter()); // 1
+console.log(coolClosureCounter()); // 2
+
